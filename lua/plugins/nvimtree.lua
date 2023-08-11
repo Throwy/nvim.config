1ac6685e259850bb92f81bad826ea79c65c0586d
Gavin Roudebush <throwy94@gmail.com>
2023-08-11 00:07:36 -0400

    Follow file with nvim-tree

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -8,7 +8,11 @@ return {
             vim.g.loaded_netrw = 1
             vim.g.loaded_netrwPlugin = 1
 
-            require("nvim-tree").setup()
+            require("nvim-tree").setup({
+                update_focused_file = {
+                    enable = true,
+                }
+            })
             vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeToggle<CR>")
         end
     }
