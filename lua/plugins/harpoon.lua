096722c497d44521d0408d1fe09ad4fb544ca9ed
Gavin Roudebush <throwy94@gmail.com>
2024-04-05 01:06:54 -0400

    Changed harpoon keymaps to alt

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,12 +12,12 @@ return {
             local ui = require("harpoon.ui")
 
             keymap.set("n", "<leader>a", mark.add_file, keymap_options({ desc = "Harpoon a file" }))
-            keymap.set("n", "<C-h>", ui.toggle_quick_menu, keymap_options({ desc = "Show harpooned list" }))
+            keymap.set("n", "<M-h>", ui.toggle_quick_menu, keymap_options({ desc = "Show harpooned list" }))
 
-            keymap.set("n", "<C-j>", function() ui.nav_file(1) end, keymap_options({ desc = "Jump to harpoon 1" }))
-            keymap.set("n", "<C-k>", function() ui.nav_file(2) end, keymap_options({ desc = "Jump to harpoon 2" }))
-            keymap.set("n", "<C-l>", function() ui.nav_file(3) end, keymap_options({ desc = "Jump to harpoon 3" }))
-            keymap.set("n", "<C-;>", function() ui.nav_file(4) end, keymap_options({ desc = "Jump to harpoon 4" }))
+            keymap.set("n", "<M-j>", function() ui.nav_file(1) end, keymap_options({ desc = "Jump to harpoon 1" }))
+            keymap.set("n", "<M-k>", function() ui.nav_file(2) end, keymap_options({ desc = "Jump to harpoon 2" }))
+            keymap.set("n", "<M-l>", function() ui.nav_file(3) end, keymap_options({ desc = "Jump to harpoon 3" }))
+            keymap.set("n", "<M-;>", function() ui.nav_file(4) end, keymap_options({ desc = "Jump to harpoon 4" }))
         end
     }
 }
