0567b852cc46eb664bc48b591bb5ff4eb6d22c03
Gavin Roudebush <throwy94@gmail.com>
2024-04-16 13:24:08 -0400

    Small improvements

--- lua/plugins/lsp_signature.lua
+++ lua/plugins/lsp_signature.lua
@@ -3,6 +3,7 @@ return {
         'ray-x/lsp_signature.nvim',
         event = "VeryLazy",
         opts = {
+            max_height = 30,
             floating_window_above_cur_line = true,
         },
         config = function(_, opts)
--- lua/throwy/set.lua
+++ lua/throwy/set.lua
@@ -40,3 +40,11 @@ opt.signcolumn = "yes"
 opt.isfname:append("@-@")
 
 opt.updatetime = 50
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('throwy-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end
+})
